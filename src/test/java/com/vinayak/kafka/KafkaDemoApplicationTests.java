2a243da08ed616d16ce6e67479a225cfda93c40c
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-08-19 11:26:07 +0530

    first commit

---
+++ src/test/java/com/vinayak/kafka/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
